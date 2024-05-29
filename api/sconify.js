eda58c5e71ad1ce2b1526b1bddaf4ecfa4c816f1
Le-Caignec <robinlecaignec@yahoo.fr>
2024-05-29 19:29:03 +0200

    fix pull, dockerode need to be authenticated

--- api/sconify.js
+++ api/sconify.js
@@ -1,41 +1,67 @@
-import Docker from 'dockerode';
+import Docker from "dockerode";
+import "dotenv/config";
 
-const SCONE_IMAGE = 'registry.scontain.com/sconecuratedimages/node:14.4.0-alpine3.11'
+const SCONE_IMAGE =
+  "registry.scontain.com/sconecuratedimages/node:14.4.0-alpine3.11";
 
+const registryAuth = {
+  username: process.env.REGISTRY_USERNAME,
+  password: process.env.REGISTRY_PASSWORD,
+  serveraddress: process.env.REGISTRY_SERVERADDRESS,
+};
 const docker = new Docker();
 
-export function sconify({
-  dockerImageToSconify
-}) {
-  // Pull Scone image
-  docker.pull(SCONE_IMAGE, function (err, stream) {
-    console.log('err', err)
-    if (err) {
-      return
-    }
+// Function to sconify the docker image
+export function sconify({ dockerImageToSconify }) {
+  console.log("Pulling Scone image...");
 
-    // console.log('-> docker.run')
-    // docker.run(SCONE_IMAGE, [
-    //   '-v /var/run/docker.sock:/var/run/docker.sock',
-    //   'registry.scontain.com/scone-production/iexec-sconify-image:5.7.6-v15',
-    //   'sconify_iexec',
-    //   '--name=my-idapp-tee-scone',
-    //   '--from=cedric25/my-idapp:0.2.0-debug',
-    //   '--to=cedric25/my-idapp:0.2.0-debug-tee-scone',
-    //   '--binary-fs',
-    //   '--fs-dir=/app',
-    //   '--host-path=/etc/hosts',
-    //   '--host-path=/etc/resolv.conf',
-    //   '--binary=/usr/local/bin/node',
-    //   '--heap=1G',
-    //   '--dlopen=1',
-    //   '--no-color',
-    //   '--verbose',
-    //   '--command=node /app/app.js',
-    // ], process.stdout, function (err, data, container) {
-    //   console.log('data', data);
-    // });
-  })
+  docker.pull(
+    SCONE_IMAGE,
+    { authconfig: registryAuth },
+    function (err, stream) {
+      if (err) {
+        console.error("Error pulling the image:", err);
+        return;
+      }
+
+      docker.modem.followProgress(stream, onFinished, onProgress);
+
+      function onFinished(err, output) {
+        if (err) {
+          console.error("Error in image pulling process:", err);
+          return;
+        }
+        console.log("Scone image pulled successfully.");
+        // Add your additional logic here, e.g., sconify the dockerImageToSconify
+      }
+
+      function onProgress(event) {
+        console.log(event.status);
+      }
+
+      // console.log('-> docker.run')
+      // docker.run(SCONE_IMAGE, [
+      //   '-v /var/run/docker.sock:/var/run/docker.sock',
+      //   'registry.scontain.com/scone-production/iexec-sconify-image:5.7.6-v15',
+      //   'sconify_iexec',
+      //   '--name=my-idapp-tee-scone',
+      //   '--from=cedric25/my-idapp:0.2.0-debug',
+      //   '--to=cedric25/my-idapp:0.2.0-debug-tee-scone',
+      //   '--binary-fs',
+      //   '--fs-dir=/app',
+      //   '--host-path=/etc/hosts',
+      //   '--host-path=/etc/resolv.conf',
+      //   '--binary=/usr/local/bin/node',
+      //   '--heap=1G',
+      //   '--dlopen=1',
+      //   '--no-color',
+      //   '--verbose',
+      //   '--command=node /app/app.js',
+      // ], process.stdout, function (err, data, container) {
+      //   console.log('data', data);
+      // });
+    }
+  );
 
   // docker.pull('cedric25/my-idapp:0.2.0-debug', function (err, stream) {
   //   // streaming output from pull...
@@ -47,5 +73,5 @@ export function sconify({
   //   });
   // });
 
-  return Promise.resolve(true)
+  return Promise.resolve(true);
 }

5063adebdeceabb8ffa1fcf87501a878ade70870
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2024-06-04 17:23:13 +0200

    Use tag "latest" if none given

--- api/src/utils/parseImagePath.js
+++ api/src/utils/parseImagePath.js
@@ -2,6 +2,6 @@ export function parseImagePath(dockerImagePath) {
   const dockerUserName = dockerImagePath.split('/')[0];
   const nameWithTag = dockerImagePath.split('/')[1];
   const imageName = nameWithTag.split(':')[0];
-  const imageTag = nameWithTag.split(':')[1];
+  const imageTag = nameWithTag.split(':')[1] || 'latest';
   return { dockerUserName, imageName, imageTag };
 }

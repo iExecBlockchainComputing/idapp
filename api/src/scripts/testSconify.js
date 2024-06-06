8f05131d219ab866db553a2e27183ac2fa560703
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2024-06-06 16:47:49 +0200

    Move testSconify script to dedicated 'scripts' folder

--- api/src/scripts/testSconify.js
+++ api/src/scripts/testSconify.js
@@ -1,8 +1,8 @@
 import 'dotenv/config';
-import { sconify } from './sconify.js';
+import { sconify } from '../sconify.js';
 
 /**
- * node src/testSconify.js
+ * node src/scripts/testSconify.js
  */
 
 function run() {
@@ -10,7 +10,8 @@ function run() {
     // dockerImageToSconify: 'cedric25/my-idapp2:0.1.0-debug',
     dockerImageToSconify: 'arthuryvars/web3telegram-app',
 
-    // cedric Metamask account 1
+    // cedric's Metamask account 1
+    // app contract will be transferred to this address at the end
     userWalletPublicAddress: '0xB151dDE0e776a64F66f46ca9E8bF20740b9b0baD',
   });
 }

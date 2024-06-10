a47fcb167783f22f91102b157316ea2c130f2490
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2024-06-10 17:25:33 +0200

    No need of specific auth to push image to dockerhub, right?

--- api/src/singleFunction/pushImage.js
+++ api/src/singleFunction/pushImage.js
@@ -2,12 +2,6 @@ import Docker from 'dockerode';
 
 const docker = new Docker();
 
-const registryAuth = {
-  username: process.env.REGISTRY_USERNAME,
-  password: process.env.REGISTRY_PASSWORD,
-  serveraddress: process.env.REGISTRY_SERVERADDRESS,
-};
-
 /**
  * @returns {Promise<{ Tag: string, Digest: string, Size: number }>}
  */
@@ -16,40 +10,37 @@ export async function pushImage({ targetImagePath, targetImageTag }) {
 
   return new Promise((resolve, reject) => {
     const img = docker.getImage(targetImagePath);
-    img.push(
-      { authconfig: registryAuth, tag: targetImageTag },
-      function (err, stream) {
-        let pushedImageResult;
+    img.push({ tag: targetImageTag }, function (err, stream) {
+      let pushedImageResult;
 
+      if (err) {
+        console.error('Error pushing the image:', err);
+        return reject(err);
+      }
+
+      docker.modem.followProgress(stream, onFinished, onProgress);
+
+      function onFinished(err, output) {
         if (err) {
-          console.error('Error pushing the image:', err);
+          console.error('Error in image pushing process:', err);
           return reject(err);
         }
+        console.log(
+          `Successfully pushed the image to DockerHub => ${targetImagePath}`
+        );
+        resolve(pushedImageResult);
+      }
 
-        docker.modem.followProgress(stream, onFinished, onProgress);
-
-        function onFinished(err, output) {
-          if (err) {
-            console.error('Error in image pushing process:', err);
-            return reject(err);
-          }
-          console.log(
-            `Successfully pushed the image to DockerHub => ${targetImagePath}`
-          );
-          resolve(pushedImageResult);
-        }
-
-        function onProgress(event) {
-          if (event.error) {
-            console.error('[img.push] onProgress ERROR', event.error);
-          } else {
-            console.log('[img.push] onProgress', event);
-            if (event.aux) {
-              pushedImageResult = event.aux;
-            }
+      function onProgress(event) {
+        if (event.error) {
+          console.error('[img.push] onProgress ERROR', event.error);
+        } else {
+          console.log('[img.push] onProgress', event);
+          if (event.aux) {
+            pushedImageResult = event.aux;
           }
         }
       }
-    );
+    });
   });
 }

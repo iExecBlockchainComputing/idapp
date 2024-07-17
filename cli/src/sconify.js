64d6cb68f5a1f50c838917e1e271f0e562835e2a
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2024-07-17 17:11:02 +0200

    Break early, production sconification not supported yet

--- cli/src/sconify.js
+++ cli/src/sconify.js
@@ -9,70 +9,64 @@ export async function sconify({
   iDappNameToSconify,
   walletAddress,
 }) {
-  let teeDockerhubImagePath = '';
-  if (!sconifyForProd) {
-    try {
-      const { body } = await request(`${SCONIFY_API_URL}/sconify`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          dockerhubImageToSconify: iDappNameToSconify,
-          yourWalletPublicAddress: walletAddress,
-        }),
-        throwOnError: true,
-      });
-      const json = await body.json();
-
-      // Extract necessary information
-      const sconifiedImage = json.sconifiedImage;
-      const appContractAddress = json.appContractAddress;
-
-      // Display the result in a beautiful format
-      mainSpinner.succeed('iDapp sconified successfully.');
-      console.log(` Sconified image: ${sconifiedImage}`);
-
-      mainSpinner.succeed('iDapp deployed');
-      console.log(` iDapp address: ${appContractAddress}`);
-
-      mainSpinner.succeed('iDapp transferred to you');
-
-      // Add deployment data to deployments.json
-      teeDockerhubImagePath = json.sconifiedImage.split(':')[0];
-      addDeploymentData({
-        sconifiedImage,
-        appContractAddress,
-        owner: walletAddress,
-      });
-    } catch (err) {
-      if (err.body) {
-        console.log('\nerr', err.body);
-      } else {
-        if (
-          err?.code === 'ECONNREFUSED' ||
-          err?.code === 'UND_ERR_CONNECT_TIMEOUT'
-        ) {
-          console.error('\n⚠️ Sconification server seems to be down!');
-        } else {
-          console.log('\nerr', err);
-        }
-      }
-      mainSpinner.fail('Failed to sconify your iDapp');
-      throw err;
-    }
+  if (sconifyForProd) {
+    console.log(
+      chalk.red('This feature is not yet implemented. Coming soon ...')
+    );
+    process.exit(0);
   }
 
-  if (sconifyForProd) {
-    try {
-      console.log(
-        chalk.red('This feature is not yet implemented. Coming soon ...')
-      );
-    } catch (e) {
-      console.log(chalk.red(e));
-      mainSpinner.fail('Failed to deploy your iDapp.');
-      throw err;
+  let teeDockerhubImagePath = '';
+  try {
+    const { body } = await request(`${SCONIFY_API_URL}/sconify`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        dockerhubImageToSconify: iDappNameToSconify,
+        yourWalletPublicAddress: walletAddress,
+      }),
+      throwOnError: true,
+    });
+    const json = await body.json();
+
+    // Extract necessary information
+    const sconifiedImage = json.sconifiedImage;
+    const appContractAddress = json.appContractAddress;
+
+    // Display the result in a beautiful format
+    mainSpinner.succeed('iDapp sconified successfully.');
+    console.log(` Sconified image: ${sconifiedImage}`);
+
+    mainSpinner.succeed('iDapp deployed');
+    console.log(` iDapp address: ${appContractAddress}`);
+
+    mainSpinner.succeed('iDapp transferred to you');
+
+    // Add deployment data to deployments.json
+    teeDockerhubImagePath = json.sconifiedImage.split(':')[0];
+    addDeploymentData({
+      sconifiedImage,
+      appContractAddress,
+      owner: walletAddress,
+    });
+  } catch (err) {
+    console.log('err', JSON.stringify(err, null, 2));
+    if (err.body) {
+      console.log('\nerr', err.body);
+    } else {
+      if (
+        err?.code === 'ECONNREFUSED' ||
+        err?.code === 'UND_ERR_CONNECT_TIMEOUT'
+      ) {
+        console.error('\n⚠️ Sconification server seems to be down!');
+      } else {
+        console.log('\nerr', err);
+      }
     }
+    mainSpinner.fail('Failed to sconify your iDapp');
+    throw err;
   }
 
   return {

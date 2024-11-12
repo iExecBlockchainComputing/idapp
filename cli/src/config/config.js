eccad35a8d366dd0a96bc0d9c1a7f4389e4920e8
pjt <26487010+PierreJeanjacquot@users.noreply.github.com>
2024-11-12 15:50:56 +0100

    use free learn pools
    default pools became payable, migrate to free learn pools

--- cli/src/config/config.js
+++ cli/src/config/config.js
@@ -1,8 +1,8 @@
 export const SCONE_TAG = ['tee', 'scone'];
 
-export const WORKERPOOL_DEBUG = 'debug-v8-bellecour.main.pools.iexec.eth';
+export const WORKERPOOL_DEBUG = 'debug-v8-learn.main.pools.iexec.eth';
 
-export const WORKERPOOL_PROD = 'prod-v8-bellecour.main.pools.iexec.eth';
+export const WORKERPOOL_PROD = 'prod-v8-learn.main.pools.iexec.eth';
 
 export const SCONIFY_API_URL =
   'http://idapp-poc.westeurope.cloudapp.azure.com:3000';

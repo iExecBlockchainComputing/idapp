3d4434dbd93c96ee462824891f3eff833f731328
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2024-11-25 17:05:40 +0100

    new line for error

--- cli/src/utils/cli-helpers.js
+++ cli/src/utils/cli-helpers.js
@@ -1,4 +1,5 @@
 export const handleCliError = ({ spinner, error }) => {
-  spinner.fail(spinner.text + `   -> ${error}`);
+  const shouldBreakLine = !spinner.text.endsWith('\n');
+  spinner.fail(spinner.text + (shouldBreakLine ? '\n' : '') + `    ${error}`);
   process.exit(1);
 };

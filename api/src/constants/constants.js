60bb83b6306bbd1c33245bafd438c9342efed565
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2024-12-03 12:00:18 +0100

    Some more comment about base linux alpine also used

--- api/src/constants/constants.js
+++ api/src/constants/constants.js
@@ -3,3 +3,7 @@ export const SCONE_NODE_IMAGE =
 
 export const SCONIFY_IMAGE =
   'registry.scontain.com/scone-production/iexec-sconify-image:5.7.6-v15';
+
+// This SCONIFY_IMAGE depends on Linux alpine:3.15
+// It will be pulled if it's not yet in the local docker
+// https://hub.docker.com/layers/library/alpine/3.15/images/sha256-6a0657acfef760bd9e293361c9b558e98e7d740ed0dffca823d17098a4ffddf5?context=explore
